fa2dd1265a5229d1b961f411587f55be98ff583c
Pixelzonetcc <cont.pixelzone@gmail.com>
2024-12-01 18:52:38 -0300

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-$conn = mysqli_connect('localhost', 'hostdeprojetos_airesstores', 'tcc@2024pz', 'hostdeprojetos_airesstores');
+$conn = mysqli_connect('localhost', 'ifhostgru_airesstoresprod', 'tcc@2024pz', 'ifhostgru_airesstoresprod');
 if (!$conn) {
     die('Connection failed: ' . mysqli_connect_error());
 }
